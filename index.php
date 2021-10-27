a45f734fe6583eeb1b532aaee83ee685458a0e4c
steftes99 <stefanotescione83@gmail.com>
2021-10-27 15:13:29 +0200

    primo oggetto movie

--- index.php
+++ index.php
@@ -5,16 +5,32 @@
         public $title;
         public $length;
         public $director;
-        public $genre;
 
-        function __construct($_length){
-            $this->length = $_length;
+        function __construct($_director){
+            $this->director = $_director;
         }
+
+        public function setLength($length){
+            if($length < 60){
+              $this->length = 'Cortometraggio'. ', '.$length . ' ' . 'minuti';
+            }
+            elseif($length > 60 && $length < 120){
+              $this->length = 'Media durata'. ', ' .$length . ' ' . 'minuti';
+            }
+            else{
+              $this->length = 'Lunga durata' . ', '.$length . ' ' . 'minuti';
+            }
+          }
+
+          public function getLength(){
+            return $this->length;
+          }
 
     }
 
-    $twows = new Movie(120);
+    $twows = new Movie('Martin Scorsese');
     $twows->title = 'The Wolf of Wall Street';
+    $twows->setLength(180);
 
     
 
