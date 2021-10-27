006cc6b2320b7a82ec4e8b744bf512a83e58ea0e
steftes99 <stefanotescione83@gmail.com>
2021-10-27 14:57:53 +0200

    prova class

--- index.php
+++ index.php
@@ -1,37 +1,17 @@
-<?php 
-    class Movie {
+<?php
+
+    class Movie{
+        
         public $title;
         public $length;
+        public $director;
+        public $genre;
 
-        //costruttore per il titolo
-
-        function __construct($_title){
-            $this->title = $_title;
-        }
-
-        //set durata film
-
-        public function setLength($duration){
-            if($duration < 60){
-                $this ->length = 'Cortometraggio';
-            }
-            elseif($duration > 60 && $duration < 120){
-                $this ->length = 'Film';
-            }
-            else {
-                $this ->length = 'Film lungo';
-            }
-        }
     }
-    $twows = new Movie('The Wolf of Wall Street');
-    $twows->setLength(180);
 
-    $sm = new Movie('Scary Movie');
-    $sm->setLength(50);
+    $twows = new Movie();
+    $twows->title = 'The Wolf of Wall Street';
 
-    $bm = new Movie('Batman - Il cavaliere oscuro');
-    $bm ->setLength(90);
+    var_dump($twows)
 
-    var_dump($twows);
-    var_dump($sm);
-    var_dump($bm);
+?>
